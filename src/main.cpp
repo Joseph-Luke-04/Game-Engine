eda291631e3cf0937bf5c99901ba40d29e5d51cf
Joseph-Luke-04 <s2424114@ed.ac.uk>
2024-02-15 16:32:27 +0000

    Rendering a Rectangle using two triangles with vertex and index data

--- src/main.cpp
+++ src/main.cpp
@@ -92,24 +92,37 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 
-        0.5f, -0.5f, 0.0f, 
-        0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  // top-right
+         0.5f, -0.5f, 0.0f,  // bottom-right
+        -0.5f, -0.5f, 0.0f,  // bottom-left
+        -0.5f,  0.5f, 0.0f   // top-left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle (so the first, second and fourth vertices respectively)
+        1, 2, 3    // second triangle (the second, third and fourth vertices)
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attribute(s)
+    glGenBuffers(1, &EBO);
+    //1. Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attribute(s)
     glBindVertexArray(VAO); 
 
-    //0. Copy our vertices array into a buffer for OpenGL to use
+    //2. Copy our vertices array into a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //1. Then set vertex attributes pointers
+    //3. Copy our index array in an element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //4. Then set vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // Polygon mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -122,7 +135,8 @@ int main() {
         //2. Use our shader program when we want to render an object
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
